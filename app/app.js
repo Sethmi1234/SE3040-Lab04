43768c472039c0347d4979e62ef41d668090a6be
Sethmi1234 <disethmi@gmail.com>
2026-02-22 13:59:09 +0530

    Add external API request using Node.js https module

--- app/app.js
+++ app/app.js
@@ -1,17 +1,14 @@
-feature-Sethmi-write-file
-const fs = require('fs'); 
-fs.writeFile('file.txt', 'Hello World!', function (err) { 
-  if (err) throw err; 
-  console.log('File saved!'); 
+const https = require('https'); 
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => { 
+  let data = ''; 
+ 
+  resp.on('data', (chunk) => { 
+    data += chunk; 
+  }); 
+ 
+  resp.on('end', () => { 
+    console.log(JSON.parse(data)); 
+  }); 
+}).on('error', (err) => { 
+  console.log("Error: " + err.message); 
 }); 
-
-
-const fs = require('fs'); 
-fs.readFile('file.txt', 'utf8', function (err, data) { 
-if (err) throw err; 
-console.log(data); 
-}); 
-
-console.log("Node.js project working!");
- main
-main
