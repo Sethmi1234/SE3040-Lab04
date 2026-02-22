2ddb0a6af445043916fdb6f6a1a70c927906201f
Sethmi1234 <disethmi@gmail.com>
2026-02-22 13:00:04 +0530

    Implement file writing functionality using Node.js fs module

--- app/app.js
+++ app/app.js
@@ -1,1 +1,5 @@
-console.log("Node.js project working!");
+const fs = require('fs'); 
+fs.writeFile('file.txt', 'Hello World!', function (err) { 
+  if (err) throw err; 
+  console.log('File saved!'); 
+}); 
