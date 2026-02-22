4be8b758edd36b6b3c37f82ada35cb7a7afb7cba
Sethmi1234 <disethmi@gmail.com>
2026-02-22 13:55:57 +0530

    Create basic HTTP web server using Node.js

--- app/app.js
+++ app/app.js
@@ -1,17 +1,6 @@
-feature-Sethmi-write-file
-const fs = require('fs'); 
-fs.writeFile('file.txt', 'Hello World!', function (err) { 
-  if (err) throw err; 
-  console.log('File saved!'); 
-}); 
-
-
-const fs = require('fs'); 
-fs.readFile('file.txt', 'utf8', function (err, data) { 
-if (err) throw err; 
-console.log(data); 
-}); 
-
-console.log("Node.js project working!");
- main
-main
+const http = require('http'); 
+http.createServer(function (req, res) { 
+  res.writeHead(200, {'Content-Type': 'text/html'}); 
+  res.write('Hello World!'); 
+  res.end(); 
+}).listen(8080); 
